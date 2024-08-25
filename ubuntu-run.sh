1674f452ec6b056800b5e36d364bb6ecd01c90d8
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-08-25 08:51:03 +0700

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -2,71 +2,11 @@
 
 # Update package lists
 sudo apt-get update -y
+sudo apt install neofetch -y
+neofetch
 
 # Install essential packages
-sudo apt-get install -y \
-  dialog \
-  bash \
-  sed \
-  wget \
-  git \
-  curl \
-  zip \
-  tar \
-  jq \
-  expect \
-  make \
-  cmake \
-  automake \
-  autoconf \
-  llvm \
-  lld \
-  lldb \
-  clang \
-  gcc \
-  binutils \
-  apt-utils \
-  bison \
-  perl \
-  gperf \
-  gawk \
-  flex \
-  bc \
-  python3 \
-  python2 \
-  python-is-python3 \
-  zstd \
-  openssl \
-  unzip \
-  cpio \
-  bison \
-  build-essential \
-  ccache \
-  liblz4-tool \
-  libsdl1.2-dev \
-  libstdc++6 \
-  libxml2 \
-  libxml2-utils \
-  lzop \
-  pngcrush \
-  schedtool \
-  squashfs-tools \
-  xsltproc \
-  zlib1g-dev \
-  libncurses5-dev \
-  bzip2 \
-  libssl-dev \
-  gcc-aarch64-linux-gnu \
-  gcc-arm-linux-gnueabihf \
-  gcc-arm-linux-gnueabi \
-  libtinfo5 \
-  dwarves \
-  libelf-dev \
-  resolvconf \
-  dos2unix \
-  kmod \
-  tmate \
-  neofetch
+
 
 # Clean up to free disk space
 df -h
